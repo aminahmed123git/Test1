e52fe7a3278fccdfaf957ba83ee6cd2b0babf313
AminASC <aminahmed_301@hotmail.com>
2024-07-11 23:16:29 +0100

    First Commit

---
+++ src/test/java/com/TestsOne/NetCompanyCi/TestCase.java
@@ -0,0 +1,24 @@
+package com.TestsOne.NetCompanyCi;
+
+public class TestCase {
+	
+	public void launchDriver() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromediver.exe");
+		driver = new Chromedriver();
+		driver.manage().timeout().multip
+		
+	}
+	
+	public void test1() {
+		
+	}
+	
+	public void test2() {
+		
+	}
+
+	public void test3() {
+	
+}
+
+}
